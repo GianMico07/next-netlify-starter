1d54ebf8d3abfebd1bce7558caa1c3d4dce30ef9
GianMico07 <48172814+GianMico07@users.noreply.github.com>
2022-07-16 18:44:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Sorry, kung naunahan ko kayo sa domain name hehe
         </p>
+        </Header>
       </main>
 
       <Footer />
